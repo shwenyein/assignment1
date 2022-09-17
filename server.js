8351d0692c4463acb135af414521e051a1fdc490
unknown <nandarshwenyein.mm199@gmail.com>
2022-09-17 22:34:17 +0630

    third commit

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const http = require('http');
+const fs = require('fs');
 
 const requestListener = function (req, res) {
-  res.writeHead(200);
-  res.end('Hello, World!');
+
+  res.writeHead(200, { 'content-type': 'text/html'});
+  // res.end('Hello, World!');
+  fs.createReadStream('information.html').pipe(res);
 }
 
 const server = http.createServer(requestListener);
 console.log("hello ");
-server.listen(8080);
+server.listen(3000);
